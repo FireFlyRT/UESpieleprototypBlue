2869b4ded56ab2e1125e43add2ccae6603642b76
FireFlyRT <starwars.98@hotmail.de>
2023-04-18 16:09:49 +0200

    Added MeshGenerationComponent Class

---
+++ Source/UESpieleprototypBlue/MeshGenerationComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "MeshGenerationComponent.h"
+#include <vector>
+
+// Sets default values for this component's properties
+UMeshGenerationComponent::UMeshGenerationComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMeshGenerationComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+int UMeshGenerationComponent::GenerateMesh(TArray<GameObject*> objects)
+{
+	
+
+
+	return 0;
+}
+
---
+++ Source/UESpieleprototypBlue/MeshGenerationComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MeshGenerationComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UESPIELEPROTOTYPBLUE_API UMeshGenerationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMeshGenerationComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION(BlueprintCallable)
+	int GenerateMesh(TArray<UStaticMesh*> meshes);
+};
